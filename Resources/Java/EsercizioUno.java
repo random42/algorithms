157c2ad5ebc66f315b1acb57be933d2d94b2c973
Roberto Sero <sandeficiere@gmail.com>
2018-05-10 11:50:22 +0200

    mergeSort implemented
    added .class ignore

--- Resources/Java/EsercizioUno.java
+++ Resources/Java/EsercizioUno.java
@@ -2,6 +2,13 @@ import java.util.*;
 
 public class EsercizioUno {
 
+  // public static void main(String[] args) {
+  //   List<Integer> list = Arrays.asList(10,2,34,23,1,-9,12,51);
+  //   List<Integer> sorted = Arrays.asList(-9,1,2,10,12,23,34,51);
+  //   ArrayList<Integer> array = new ArrayList<Integer>(list);
+  //   EsercizioUno.mergeSort(array);
+  // }
+
   public static <T extends Comparable<T>> void insertionSort(ArrayList<T> array) {
     if (array == null || array.size() <= 1) {
       return;
@@ -15,6 +22,60 @@ public class EsercizioUno {
           j--;
         }
         array.set(j+1, value);
+      }
+    }
+  }
+
+  public static <T extends Comparable<T>> void mergeSort(ArrayList<T> array) {
+    if (array == null || array.size() == 0) {
+      return;
+    }
+    else {
+      int length = array.size();
+      int subLength = 2;
+      int left = length - 2;
+      int right = length;
+      boolean end = false;
+      while (!end) {
+        end = subLength >= length;
+        while (right > 0) {
+          if (left < 0) left = 0;
+          if (right - left > subLength / 2) {
+            int center = right - subLength/2;
+            merge(array,left,right,center);
+          }
+          left -= subLength;
+          right -= subLength;
+        }
+        subLength = subLength << 1;
+        right = length;
+        left = right - subLength;
+      }
+    }
+  }
+
+  protected static <T extends Comparable<T>> void merge(ArrayList<T> array, int left, int right, int center) {
+    ArrayList<T> temp = new ArrayList<T>(array.subList(left,right));
+    center -= left;
+    int length = right - left;
+    int firstCursor = 0;
+    int secCursor = center;
+    for (int i = left; i < right;i++) {
+      // if cursors are valid compare
+      if (firstCursor < center && secCursor < length) {
+        if (temp.get(secCursor).compareTo(temp.get(firstCursor)) > 0) {
+          array.set(i, temp.get(firstCursor++));
+        } else {
+          array.set(i, temp.get(secCursor++));
+        }
+      }
+      // else use valid cursor
+      else {
+        if (firstCursor == center) {
+          array.set(i, temp.get(secCursor++));
+        } else {
+          array.set(i, temp.get(firstCursor++));
+        }
       }
     }
   }
--- Resources/Java/EsercizioUnoTest.java
+++ Resources/Java/EsercizioUnoTest.java
@@ -4,12 +4,46 @@ import java.util.*;
 
 public class EsercizioUnoTest {
   @Test
-  public void testInsertionSort() {
-    List<Integer> array = Arrays.asList(1,5,7,2,4,800,35,12,3);
+  public void insertionSortInteger() {
+    List<Integer> list = Arrays.asList(1,5,7,2,4,800,35,12,3);
     List<Integer> sorted = Arrays.asList(1,2,3,4,5,7,12,35,800);
-    ArrayList<Integer> list = new ArrayList<Integer>(9);
-    list.addAll(array);
-    EsercizioUno.insertionSort(list);
-    assertEquals(sorted, list);
+    ArrayList<Integer> array = new ArrayList<Integer>(list);
+    EsercizioUno.insertionSort(array);
+    assertEquals(sorted, array);
+  }
+
+  @Test
+  public void insertionSortString() {
+    List<String> list = Arrays.asList("bacco","dodici","a","aaaa","zeta");
+    List<String> sorted = Arrays.asList("a","aaaa","bacco","dodici","zeta");
+    ArrayList<String> array = new ArrayList<String>(list);
+    EsercizioUno.insertionSort(array);
+    assertEquals(sorted, array);
+  }
+
+  @Test
+  public void mergeSortInteger() {
+    List<Integer> list = Arrays.asList(13,12,20,100,1,-30,23,11,9,30);
+    List<Integer> sorted = Arrays.asList(-30,1,9,11,12,13,20,23,30,100);
+    ArrayList<Integer> array = new ArrayList<Integer>(list);
+    EsercizioUno.mergeSort(array);
+    assertEquals(sorted, array);
+  }
+
+  @Test
+  public void mergeSortString() {
+    List<String> list = Arrays.asList("bacco","dodici","a","aaaa","zeta");
+    List<String> sorted = Arrays.asList("a","aaaa","bacco","dodici","zeta");
+    ArrayList<String> array = new ArrayList<String>(list);
+    EsercizioUno.mergeSort(array);
+    assertEquals(sorted, array);
+  }
+
+  @Test
+  public void mergeTest() {
+    ArrayList<Integer> a = new ArrayList<Integer>(Arrays.asList(2,10,12,1,15,20));
+    List<Integer> sorted = Arrays.asList(1,2,10,12,15,20);
+    EsercizioUno.merge(a,0,6,3);
+    assertEquals(a,sorted);
   }
 }
